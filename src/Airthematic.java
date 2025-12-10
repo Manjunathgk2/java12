f26afb2335d01ba59d89f0a0e7bd0ae2b5affa44
mgky <manjunathgk16198@gmail.com>
2025-12-10 19:24:44 +0530

    second  commit

--- src/Airthematic.java
+++ src/Airthematic.java
@@ -2,11 +2,8 @@ import java.util.Scanner;
 
 public class Airthematic {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter a value");
-        int a=sc.nextInt();
-        System.out.println("enter b value");
-        int b=sc.nextInt();
+        int a=15;
+        int b=12;
        int r=(a>b)?a:b;
        System.out.println(r);
 
