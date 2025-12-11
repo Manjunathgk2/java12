8932b4003b2beedb0302749e33eaccb6aeaf49f1
mgky <manjunathgk16198@gmail.com>
2025-12-11 10:40:55 +0530

    fourth commit

--- src/Datatypes1.java
+++ src/Datatypes1.java
@@ -1,16 +1,10 @@
 public class Datatypes1 {
     public static void main(String[] args) {
-        boolean b=true;
-        byte a=12;
-        short s=123;
         int i=12345;
         long l=12345;
         float f=6.345f;
         double d=12.34566;
         char c='a';
-        System.out.println(b);
-        System.out.println(a);
-        System.out.println(s);
         System.out.println(i);
         System.out.println(l);
         System.out.println(f);
